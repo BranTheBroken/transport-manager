01fe34ff1a8899a97b3f2b2ee27fc7c227419440
Dawid <d.drapinski@proton.me>
2023-07-13 21:22:21 +0200

    init config

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-duplicate-imports': 'error',
+    '@typescript-eslint/lines-between-class-members': [
+      'warn',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    '@typescript-eslint/padding-line-between-statements': [
+      'warn',
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'import'],
+        next: [
+          'block-like',
+          'break',
+          'class',
+          'continue',
+          'do',
+          'export',
+          'for',
+          'if',
+          'iife',
+          'return',
+          'switch',
+          'throw',
+          'try',
+          'while',
+          'with',
+        ],
+      },
+    ],
+    'eol-last': 'warn',
+    'dot-location': ['warn', 'property'],
+    'prefer-destructuring': [
+      'warn',
+      { array: true, object: true },
+      { enforceForRenamedProperties: true },
+    ],
+  },
+};
